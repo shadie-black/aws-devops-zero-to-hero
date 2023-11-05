a0ee2ff3152d9aa25f29db3fde73f46eeb0bd60e
shadie-black <72304640+shadie-black@users.noreply.github.com>
2023-11-05 07:23:35 +0100

    update the start container

--- scripts/start_container.sh
+++ scripts/start_container.sh
@@ -5,4 +5,4 @@ set -e
 docker pull shadietech/simple-python-flask-app:latest
 
 # Run the Docker image as a container
-docker run -d -p 5000:5000 shadietec/simple-python-flask-app
+docker run -d -p 5000:5000 shadietec/simple-python-flask-app   
